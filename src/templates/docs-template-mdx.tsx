5336158646c462a1e33906347efc73a2a24eccdc
Ugonna William Imoh <williamimoh@Ugonnas-Air.localdomain>
2021-11-30 18:00:51 +0100

    update

--- src/templates/docs-template-mdx.tsx
+++ src/templates/docs-template-mdx.tsx
@@ -33,7 +33,7 @@ const BlogPostMdx = ({ data }: any) => {
         description={doc.frontmatter.meta_description}
         lang="en"
       />
-      <Text fontSize={"3xl"} fontWeight={"bold"}>
+      <Text as={'h1'} fontSize={"3xl"} fontWeight={"bold"}>
         {doc.frontmatter.title}
       </Text>
 
